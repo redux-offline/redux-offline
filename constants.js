762cc1c729d7f892d262a9a90ef599c2fd44d944
Hajji Tarik <hajji-tarik@outlook.fr>
2017-03-29 13:35:42 +0100

    Create constants.js

---
+++ constants.js
@@ -0,0 +1,4 @@
+export const OFFLINE_STATUS_CHANGED = 'Offline/STATUS_CHANGED';
+export const OFFLINE_SCHEDULE_RETRY = 'Offline/SCHEDULE_RETRY';
+export const OFFLINE_COMPLETE_RETRY = 'Offline/COMPLETE_RETRY';
+export const OFFLINE_SEND = 'Offline/SEND';
