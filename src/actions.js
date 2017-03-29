b84cf24210e169be351d4e70b88837a8d33954b5
Hajji Tarik <hajji-tarik@outlook.fr>
2017-03-29 13:37:12 +0100

    Update actions.js

--- src/actions.js
+++ src/actions.js
@@ -1,6 +1,21 @@
+import * as CONSTANTS from 'constants';
+
 export const networkStatusChanged = online => ({
-  type: 'Offline/STATUS_CHANGED',
+  type: CONSTANTS.OFFLINE_STATUS_CHANGED,
   payload: {
     online
   }
+});
+
+export const scheduleRetry = (delay = 0) => ({
+  type: CONSTANTS.OFFLINE_SCHEDULE_RETRY,
+  payload: {
+    delay
+  }
+});
+
+export const completeRetry = (action, retryToken) => ({
+  type: CONSTANTS.OFFLINE_COMPLETE_RETRY,
+  payload: action,
+  meta: { retryToken }
 });
