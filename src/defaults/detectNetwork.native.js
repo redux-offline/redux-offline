ac83f3e12199be3fd57105a2bd7746e9ba40c788
j8seangel <jseangel@gmail.com>
2017-06-06 10:57:51 +0200

    Check differences on network changes

--- src/defaults/detectNetwork.native.js
+++ src/defaults/detectNetwork.native.js
@@ -13,65 +13,108 @@ class DetectNetwork {
   }
 
   /**
-   * Sets the connection reachability prop
-   * @param reach - connection reachability.
+   * Check props for changes
+   * @param {string} reach - connection reachability.
    *     - iOS: [none, wifi, cell, unknown]
-   *     - Android: [NONE, BLUETOOTH, DUMMY, ETHERNET, MOBILE, MOBILE_DUN, MOBILE_HIPRI, MOBILE_MMS, MOBILE_SUPL, VPN, WIFI, WIMAX, UNKNOWN]
+   *     - Android: [NONE, BLUETOOTH, DUMMY, ETHERNET, MOBILE, MOBILE_DUN, MOBILE_HIPRI,
+   *                MOBILE_MMS, MOBILE_SUPL, VPN, WIFI, WIMAX, UNKNOWN]
+   * @returns {boolean} - Whether the connection reachability or the connection props have changed
    * @private
    */
-  _setReach = (reach) => {
-    this._reach = reach.toUpperCase();
-    this._setIsConnected(reach.toUpperCase());
+  _hasChanged = reach => {
+    if (this._reach !== reach) {
+      return true;
+    }
+    if (this._isConnected !== this._getConnection(reach)) {
+      return true;
+    }
+    return false;
   }
   /**
-   * Sets the isConnected prop depending on the connection reachability's value
-   * @param reach
+   * Sets the connection reachability prop
+   * @param {string} reach - connection reachability.
+   *     - iOS: [none, wifi, cell, unknown]
+   *     - Android: [NONE, BLUETOOTH, DUMMY, ETHERNET, MOBILE, MOBILE_DUN, MOBILE_HIPRI,
+   *                MOBILE_MMS, MOBILE_SUPL, VPN, WIFI, WIMAX, UNKNOWN]
+   * @returns {void}
    * @private
    */
-  _setIsConnected = (reach) => {
-    this._isConnected = (reach !== 'NONE' && reach !== 'UNKNOWN');
+  _setReach = reach => {
+    this._reach = reach;
+    this._isConnected = this._getConnection(reach);
+  }
+  /**
+   * Gets the isConnected prop depending on the connection reachability's value
+   * @param {string} reach - connection reachability.
+   *     - iOS: [none, wifi, cell, unknown]
+   *     - Android: [NONE, BLUETOOTH, DUMMY, ETHERNET, MOBILE, MOBILE_DUN, MOBILE_HIPRI,
+   *                MOBILE_MMS, MOBILE_SUPL, VPN, WIFI, WIMAX, UNKNOWN]
+   * @returns {void}
+   * @private
+   */
+  _getConnection = reach => {
+    return reach !== 'NONE' && reach !== 'UNKNOWN';
   }
   /**
    * Sets the isConnectionExpensive prop
-   * @returns {Promise.<void>}
+   * @returns {Promise.<void>} Resolves to true if connection is expensive,
+   * false if not, and null if not supported.
    * @private
    */
   _setIsConnectionExpensive = async () => {
     try {
       this._isConnectionExpensive = await NetInfo.isConnectionExpensive();
     } catch (err) {
-      // err means that isConnectionExpensive is not supported
+      // err means that isConnectionExpensive is not supported in iOS
       this._isConnectionExpensive = null;
     }
   }
   /**
    * Fetches and sets the connection reachability and the isConnected props
-   * @returns {Promise.<void>}
+   * @returns {Promise.<void>} Resolves when the props have been
+   * initialized and update.
    * @private
    */
   _init = async () => {
-    this._setReach(await NetInfo.fetch());
-    this._updateState();
+    const reach = await NetInfo.fetch();
+    this._update(reach);
+  }
+  /**
+   * Check changes on props and store and dispatch if neccesary
+   * @param {string} reach - connection reachability.
+   *     - iOS: [none, wifi, cell, unknown]
+   *     - Android: [NONE, BLUETOOTH, DUMMY, ETHERNET, MOBILE, MOBILE_DUN, MOBILE_HIPRI,
+   *                MOBILE_MMS, MOBILE_SUPL, VPN, WIFI, WIMAX, UNKNOWN]
+   * @returns {void}
+   * @private
+   */
+  _update = reach => {
+    const normalizedReach = reach.toUpperCase();
+    if (this._hasChanged(normalizedReach)) {
+      this._setReach(normalizedReach);
+      this._dispatch();
+    }
   }
 
   /**
    * Adds listeners for when connection reachability and app state changes to update props
+   * @returns {void}
    * @private
    */
   _addListeners() {
     NetInfo.addEventListener('change', (reach) => {
-      this._setReach(reach);
-      this._updateState();
+      this._update(reach);
     });
     AppState.addEventListener('change', this._init);
   }
 
   /**
    * Executes the given callback to update redux's store with the new internal props
-   * @returns {Promise.<void>}
+   * @returns {Promise.<void>} Resolves after fetching the isConnectionExpensive
+   * and dispatches actions
    * @private
    */
-  _updateState = async () => {
+  _dispatch = async () => {
     await this._setIsConnectionExpensive();
     this._callback({
       online: this._isConnected,
