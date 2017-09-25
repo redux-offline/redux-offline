661d54258bfded53bc556bfc3a51f6e805458444
Sebastian Garrido <sebasgarcep@gmail.com>
2017-09-25 16:07:27 -0500

    feat: warn user if defaultCommit or defaultRollback are not proper redux actions

--- src/index.js
+++ src/index.js
@@ -9,6 +9,23 @@ import { applyDefaults } from './config';
 import { networkStatusChanged } from './actions';
 
 // @TODO: Take createStore as config?
+const warnIfNotReduxAction = (config: $Shape<Config>, key: string) => {
+  const maybeAction = config[key];
+
+  const isNotReduxAction =
+    maybeAction === null ||
+    typeof maybeAction !== 'object' ||
+    typeof maybeAction.type !== 'string' ||
+    maybeAction.type === '';
+
+  if (isNotReduxAction && console.warn) {
+    const msg =
+      `${key} must be a proper redux action, ` +
+      `i.e. it must be an object and have a non-empty string type. ` +
+      `Instead you provided: ${JSON.stringify(maybeAction, null, 2)}`;
+    console.warn(msg);
+  }
+};
 
 // eslint-disable-next-line no-unused-vars
 let persistor;
@@ -17,6 +34,9 @@ export const offline = (userConfig: $Shape<Config> = {}) => (
   createStore: any
 ) => (reducer: any, preloadedState: any, enhancer: any = x => x) => {
   const config = applyDefaults(userConfig);
+
+  warnIfNotReduxAction(config, 'defaultCommit');
+  warnIfNotReduxAction(config, 'defaultRollback');
 
   // wraps userland reducer with a top-level
   // reducer that handles offline state updating
