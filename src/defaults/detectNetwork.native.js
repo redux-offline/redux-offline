236eae095e7cf4d0b8045bc416f33c10bf95cdf8
Kim Brandwijk <kim.brandwijk@gmail.com>
2017-10-15 23:00:51 +0200

    Updated deprecated NetInfo usage

--- src/defaults/detectNetwork.native.js
+++ src/defaults/detectNetwork.native.js
@@ -15,9 +15,8 @@ class DetectNetwork {
   /**
    * Check props for changes
    * @param {string} reach - connection reachability.
-   *     - iOS: [none, wifi, cell, unknown]
-   *     - Android: [NONE, BLUETOOTH, DUMMY, ETHERNET, MOBILE, MOBILE_DUN, MOBILE_HIPRI,
-   *                MOBILE_MMS, MOBILE_SUPL, VPN, WIFI, WIMAX, UNKNOWN]
+   *     - Cross-platform: [none, wifi, cellular, unknown]
+   *     - Android: [bluetooth, ethernet, wimax]
    * @returns {boolean} - Whether the connection reachability or the connection props have changed
    * @private
    */
@@ -33,9 +32,8 @@ class DetectNetwork {
   /**
    * Sets the connection reachability prop
    * @param {string} reach - connection reachability.
-   *     - iOS: [none, wifi, cell, unknown]
-   *     - Android: [NONE, BLUETOOTH, DUMMY, ETHERNET, MOBILE, MOBILE_DUN, MOBILE_HIPRI,
-   *                MOBILE_MMS, MOBILE_SUPL, VPN, WIFI, WIMAX, UNKNOWN]
+   *     - Cross-platform: [none, wifi, cellular, unknown]
+   *     - Android: [bluetooth, ethernet, wimax]
    * @returns {void}
    * @private
    */
@@ -46,13 +44,12 @@ class DetectNetwork {
   /**
    * Gets the isConnected prop depending on the connection reachability's value
    * @param {string} reach - connection reachability.
-   *     - iOS: [none, wifi, cell, unknown]
-   *     - Android: [NONE, BLUETOOTH, DUMMY, ETHERNET, MOBILE, MOBILE_DUN, MOBILE_HIPRI,
-   *                MOBILE_MMS, MOBILE_SUPL, VPN, WIFI, WIMAX, UNKNOWN]
+   *     - Cross-platform: [none, wifi, cellular, unknown]
+   *     - Android: [bluetooth, ethernet, wimax]
    * @returns {void}
    * @private
    */
-  _getConnection = reach => reach !== 'NONE' && reach !== 'UNKNOWN';
+  _getConnection = reach => reach !== 'none' && reach !== 'unknown';
   /**
    * Sets the isConnectionExpensive prop
    * @returns {Promise.<void>} Resolves to true if connection is expensive,
@@ -74,22 +71,20 @@ class DetectNetwork {
    * @private
    */
   _init = async () => {
-    const reach = await NetInfo.fetch();
-    this._update(reach);
+    const connectionInfo = await NetInfo.getConnectionInfo();
+    this._update(connectionInfo.type);
   };
   /**
    * Check changes on props and store and dispatch if neccesary
    * @param {string} reach - connection reachability.
-   *     - iOS: [none, wifi, cell, unknown]
-   *     - Android: [NONE, BLUETOOTH, DUMMY, ETHERNET, MOBILE, MOBILE_DUN, MOBILE_HIPRI,
-   *                MOBILE_MMS, MOBILE_SUPL, VPN, WIFI, WIMAX, UNKNOWN]
+   *     - Cross-platform: [none, wifi, cellular, unknown]
+   *     - Android: [bluetooth, ethernet, wimax]
    * @returns {void}
    * @private
    */
   _update = reach => {
-    const normalizedReach = reach.toUpperCase();
-    if (this._hasChanged(normalizedReach)) {
-      this._setReach(normalizedReach);
+    if (this._hasChanged(reach)) {
+      this._setReach(reach);
       this._dispatch();
     }
   };
@@ -100,8 +95,8 @@ class DetectNetwork {
    * @private
    */
   _addListeners() {
-    NetInfo.addEventListener('change', reach => {
-      this._update(reach);
+    NetInfo.addEventListener('connectionChange', connectionInfo => {
+      this._update(connectionInfo.type);
     });
     AppState.addEventListener('change', this._init);
   }
