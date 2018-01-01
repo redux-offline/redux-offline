92fa3982d0afc998e1ef0c4a531083ace5c0ea05
Unknown <paul.brachmann@malpaux.com>
2018-01-01 22:12:01 +0100

    Update TypeScript type declarations

--- typings.d.ts
+++ typings.d.ts
@@ -1,6 +1,11 @@
-declare module 'redux-offline' {
-  import { createStore as createReduxStore, Store, StoreEnhancer } from 'redux';
+declare module '@redux-offline/redux-offline/lib/defaults' {
+  import { Config } from '@redux-offline/redux-offline/lib/types';
 
+  const config: Config;
+  export default config;
+}
+
+declare module '@redux-offline/redux-offline/lib/types' {
   export interface ResultAction {
     meta: {
       completed: boolean;
@@ -16,12 +21,6 @@ declare module 'redux-offline' {
     rollback: ResultAction;
   }
 
-  export interface Receipt {
-    message: OfflineMetadata;
-    result: object;
-    success: boolean;
-  }
-
   export interface OfflineAction {
     meta: {
       offline: OfflineMetadata;
@@ -31,47 +30,84 @@ declare module 'redux-offline' {
     type: string;
   }
 
+  export interface NetInfo {
+    isConnectionExpensive?: boolean;
+    reach: string;
+  }
+
+  export interface OfflineStatusChangeAction {
+    payload: {
+      netInfo?: NetInfo;
+      online: boolean;
+    };
+    type: string;
+  }
+
+  export interface OfflineScheduleRetryAction {
+    type: string;
+  }
+
   export type Outbox = OfflineAction[];
 
   export interface OfflineState {
+    busy: boolean;
     lastTransaction: number;
+    netInfo?: NetInfo;
     online: boolean;
     outbox: Outbox;
-    receipts: Receipt[];
     retryCount: number;
     retryScheduled: boolean;
-    retryToken: number;
+  }
+
+  export interface PersistRehydrateAction {
+    payload: {
+      offline: OfflineState;
+    };
+    type: string;
   }
 
   export interface AppState {
     offline: OfflineState;
   }
 
-  type NetworkCallback = (result: boolean) => void;
+  export type NetworkCallback = (result: boolean) => void;
 
   export interface Config {
-    batch: (outbox: Outbox) => Outbox;
+    defaultCommit: { type: string };
+    defaultRollback: { type: string };
     detectNetwork: (callback: NetworkCallback) => void;
     discard: (error: any, action: OfflineAction, retries: number) => boolean;
     effect: (effect: any, action: OfflineAction) => Promise<any>;
+    offlineStateLens: (
+      state: any,
+    ) => { get: OfflineState, set: (offlineState?: OfflineState) => any };
     persist: (store: any) => any;
-    persistCallback: () => any;
+    persistAutoRehydrate: (config?: { [key: string]: any }) => (next: any) => any;
+    persistCallback: (callback?: any) => any;
     persistOptions: { [key: string]: any };
-    rehydrate?: boolean;
     retry: (action: OfflineAction, retries: number) => number | void;
   }
+}
 
-  export const offline: (config: Config) => (createStore: typeof createReduxStore) =>
+declare module '@redux-offline/redux-offline' {
+  import { createStore as createReduxStore, Store, StoreEnhancer } from 'redux';
+
+  import { Config } from '@redux-offline/redux-offline/lib/types';
+
+  export const offline: (userConfig: Config) => (createStore: typeof createReduxStore) =>
     <T extends { [key: string]: any }>(
       reducer: (state: T, action: any) => T,
       preloadedState: T,
       enhancer: StoreEnhancer<T>,
     ) => Store<T>;
-}
 
-declare module 'redux-offline/lib/defaults' {
-  import { Config } from 'redux-offline';
-
-  const config: Config;
-  export default config;
+  export const createOffline: (userConfig: Config) => ({
+    enhanceReducer: (reducer: any) => (state: any, action: any) => any,
+    enhanceStore: (next: any) => <T extends { [key: string]: any }>(
+      reducer: (state: T, action: any) => T,
+      preloadedState: T,
+      enhancer: StoreEnhancer<T>,
+    ) => Store<T>,
+    middleware: <A = any, R = any>(store: any) => (next: (action: A) => R) => (action: A) => R,
+  });
 }
