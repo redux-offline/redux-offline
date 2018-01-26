9a315bce2b83bd8710bd10822b92e1dc3d71f8a0
David Escalera <david.escalera.ponce@gmail.com>
2018-01-26 23:21:46 +0100

    Travis fixings

--- src/thunk-middleware.js
+++ src/thunk-middleware.js
@@ -1,15 +1,14 @@
-
 const reduxOfflineThunkMiddleware = thunks => store => next => action => {
-  const result = next(action)
+  const result = next(action);
 
-  if (typeof action === 'object'
-    && action.meta && action.meta.thunk
-    && typeof action.meta.thunk === 'object'
+  if (typeof action === 'object' &&
+    action.meta && action.meta.thunk &&
+    typeof action.meta.thunk === 'object'
   ) {
-    store.dispatch(thunks[action.meta.thunk.functionName].apply(null, action.meta.thunk.params))
+    store.dispatch(thunks[action.meta.thunk.functionName].apply(null, action.meta.thunk.params));
   }
   
-  return result
+  return result;
 }
 
-export default reduxOfflineThunkMiddleware
+export default reduxOfflineThunkMiddleware;
