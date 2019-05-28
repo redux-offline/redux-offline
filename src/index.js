8eff6b1ad166c2e008f427bc4b21c0e060c26581
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2019-05-28 13:03:18 +0200

    Unified duplicated code

--- src/index.js
+++ src/index.js
@@ -30,16 +30,7 @@ const warnIfNotReduxAction = (config: $Shape<Config>, key: string) => {
 export const offline = (userConfig: $Shape<Config> = {}) => (
   createStore: any
 ) => (reducer: any, preloadedState: any, enhancer: any = x => x) => {
-  const config = applyDefaults(userConfig);
-
-  warnIfNotReduxAction(config, 'defaultCommit');
-  warnIfNotReduxAction(config, 'defaultRollback');
-
-  // toggle experimental returned promises
-  config.offlineActionTracker = config.returnPromises
-    ? offlineActionTracker.withPromises
-    : offlineActionTracker.withoutPromises;
-  delete config.returnPromises;
+  const config = mergeConfigs(userConfig);
 
   // wraps userland reducer with a top-level
   // reducer that handles offline state updating
@@ -58,12 +49,58 @@ export const offline = (userConfig: $Shape<Config> = {}) => (
       : offlineMiddleware;
 
   // create store
-  const store = offlineEnhancer(createStore)(
+  return configStore(offlineEnhancer(createStore)(
     offlineReducer,
     preloadedState,
     enhancer
-  );
+  ), config);
+};
 
+export const createOffline = (userConfig: $Shape<Config> = {}) => {
+  const config = mergeConfigs(userConfig);
+
+  const enhanceStore = (next: any) => (
+    reducer: any,
+    preloadedState: any,
+    enhancer: any
+  ) => {
+    // create autoRehydrate enhancer if required
+    const createStore =
+      config.persist && config.rehydrate && config.persistAutoRehydrate
+        ? config.persistAutoRehydrate()(next)
+        : next;
+
+    // create store
+    return configStore(createStore(reducer, preloadedState, enhancer), config);
+  };
+
+  return {
+    middleware: createOfflineMiddleware(config),
+    enhanceReducer(reducer: any) {
+      return enhanceReducer(reducer, config);
+    },
+    enhanceStore
+  };
+};
+
+function mergeConfigs(userConfig)
+{
+  const config = applyDefaults(userConfig);
+
+  warnIfNotReduxAction(config, 'defaultCommit');
+  warnIfNotReduxAction(config, 'defaultRollback');
+
+  // toggle experimental returned promises
+  config.offlineActionTracker = config.returnPromises
+    ? offlineActionTracker.withPromises
+    : offlineActionTracker.withoutPromises;
+  delete config.returnPromises;
+
+  return config
+}
+
+function configStore(store, config)
+{
   const baseReplaceReducer = store.replaceReducer.bind(store);
   // $FlowFixMe
   store.replaceReducer = function replaceReducer(nextReducer) {
@@ -83,59 +120,4 @@ export const offline = (userConfig: $Shape<Config> = {}) => (
   }
 
   return store;
-};
-
-export const createOffline = (userConfig: $Shape<Config> = {}) => {
-  const config = applyDefaults(userConfig);
-
-  // toggle experimental returned promises
-  config.offlineActionTracker = config.returnPromises
-    ? offlineActionTracker.withPromises
-    : offlineActionTracker.withoutPromises;
-  delete config.returnPromises;
-
-  warnIfNotReduxAction(config, 'defaultCommit');
-  warnIfNotReduxAction(config, 'defaultRollback');
-
-  const enhanceStore = (next: any) => (
-    reducer: any,
-    preloadedState: any,
-    enhancer: any
-  ) => {
-    // create autoRehydrate enhancer if required
-    const createStore =
-      config.persist && config.rehydrate && config.persistAutoRehydrate
-        ? config.persistAutoRehydrate()(next)
-        : next;
-
-    // create store
-    const store = createStore(reducer, preloadedState, enhancer);
-
-    const baseReplaceReducer = store.replaceReducer.bind(store);
-    store.replaceReducer = function replaceReducer(nextReducer) {
-      return baseReplaceReducer(enhanceReducer(nextReducer, config));
-    };
-
-    // launch store persistor
-    if (config.persist) {
-      config.persist(store, config.persistOptions, config.persistCallback);
-    }
-
-    // launch network detector
-    if (config.detectNetwork) {
-      config.detectNetwork(online => {
-        store.dispatch(networkStatusChanged(online));
-      });
-    }
-
-    return store;
-  };
-
-  return {
-    middleware: createOfflineMiddleware(config),
-    enhanceReducer(reducer: any) {
-      return enhanceReducer(reducer, config);
-    },
-    enhanceStore
-  };
-};
+}
