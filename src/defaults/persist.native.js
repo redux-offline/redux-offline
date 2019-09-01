9764ca8f6b79942cb9f62c10229b0444416f0f90
sorodrigo <rodrigo.solis@vizzuality.com>
2019-09-01 13:20:46 +0200

    remove async-storage from package.json

--- src/defaults/persist.native.js
+++ src/defaults/persist.native.js
@@ -2,7 +2,7 @@
 
 // $FlowIgnore
 import { persistStore } from 'redux-persist';
-import AsyncStorage from '@react-native-community/async-storage';
+import AsyncStorage from '@react-native-community/async-storage'; // eslint-disable-line
 
 export default (store: any, options: {}, callback: any) =>
   persistStore(store, { storage: AsyncStorage, ...options }, callback);
