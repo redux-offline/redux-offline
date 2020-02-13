75df50a3538e3c108518ec92c245fb418d110b32
Alex <asobiloff@gmail.com>
2020-02-13 15:58:54 -0800

    removed file that prevented alternative storage engines

--- src/defaults/persist.native.js
+++
@@ -1,8 +0,0 @@
-// @flow
-
-// $FlowIgnore
-import { AsyncStorage } from 'react-native'; // eslint-disable-line
-import { persistStore } from 'redux-persist';
-
-export default (store: any, options: {}, callback: any) =>
-  persistStore(store, { storage: AsyncStorage, ...options }, callback);
