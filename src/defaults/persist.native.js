f2cda0605394fe0b652cc2c9ba3371a1e7b72920
rpweb <github@rp-webstudios.com>
2020-04-25 19:51:17 +0200

    expo 37 workaround

--- src/defaults/persist.native.js
+++ src/defaults/persist.native.js
@@ -1,8 +1,8 @@
 // @flow
 
 // $FlowIgnore
-import { persistStore } from 'redux-persist';
-import AsyncStorage from '@react-native-community/async-storage'; // eslint-disable-line
+import { persistStore } from "redux-persist";
+import { AsyncStorage } from "react-native"; // eslint-disable-line
 
 export default (store: any, options: {}, callback: any) =>
   persistStore(store, { storage: AsyncStorage, ...options }, callback);
