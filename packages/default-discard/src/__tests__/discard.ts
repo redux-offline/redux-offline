34a01ae07b1a6cad4c0b6460eed3ede472b7f788
Fran Bosquet <frbosquet@gmail.com>
2021-10-28 15:21:08 +0200

    standarize meta along tests

--- packages/default-discard/src/__tests__/discard.ts
+++ packages/default-discard/src/__tests__/discard.ts
@@ -2,33 +2,34 @@ import discard from '../discard';
 
 const meta = {
   commit: {},
-  rollback: {}
+  rollback: {},
+  effect: 'any_effect'
 }
 
 test('retries non-http error once', () => {
   const error = { message: 'Non-Http error' };
-  const action = { meta: {...meta, effect: 'SIDE_EFFECT' } };
+  const action = { meta };
 
   expect(discard(error, action)).toEqual(false);
 });
 
 test('discards non-http after one retry', () => {
   const error = { message: 'Non-Http error' };
-  const action = { meta: {...meta, effect: 'SIDE_EFFECT' } };
+  const action = { meta };
 
   expect(discard(error, action, 1)).toEqual(true);
 });
 
 test('discards http 4xx errors', () => {
   const error = { status: 404 };
-  const action = { meta: { ...meta, effect: "any_effect" } };
+  const action = { meta };
 
   expect(discard(error, action)).toEqual(true);
 });
 
 test('does not discard http 5xx errors', () => {
   const error = { status: 500 };
-  const action = { meta: { ...meta, effect: "any_effect" } };
+  const action = { meta };
 
   expect(discard(error, action)).toEqual(false);
 });
