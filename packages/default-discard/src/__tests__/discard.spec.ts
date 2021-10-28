2a27ec96f659cd67b6710df4cb2adc5f75f754dc
Fran Bosquet <frbosquet@gmail.com>
2021-10-28 15:10:51 +0200

    update default-discard tests and types

--- packages/default-discard/src/__tests__/discard.spec.ts
+++ packages/default-discard/src/__tests__/discard.spec.ts
@@ -1,29 +1,34 @@
 import discard from '../discard';
 
+const meta = {
+  commit: {},
+  rollback: {}
+}
+
 test('retries non-http error once', () => {
-  const error = { message: 'Non-http error' };
-  const action = { type: 'DISCARD', meta: { offline: { effect: {} } } };
+  const error = { message: 'Non-Http error' };
+  const action = { meta: {...meta, effect: 'SIDE_EFFECT' } };
 
   expect(discard(error, action)).toEqual(false);
 });
 
-test('discards non-http error after one retry', () => {
-  const error = { message: 'Non-http error' };
-  const action = { type: 'DISCARD', meta: { offline: { effect: {} } } };
+test('discards non-http after one retry', () => {
+  const error = { message: 'Non-Http error' };
+  const action = { meta: {...meta, effect: 'SIDE_EFFECT' } };
 
   expect(discard(error, action, 1)).toEqual(true);
 });
 
 test('discards http 4xx errors', () => {
   const error = { status: 404 };
-  const action = { type: 'DISCARD', meta: { offline: { effect: {} } } };
+  const action = { meta: { ...meta, effect: "any_effect" } };
 
   expect(discard(error, action)).toEqual(true);
 });
 
 test('does not discard http 5xx errors', () => {
   const error = { status: 500 };
-  const action = { type: 'DISCARD', meta: { offline: { effect: {} } } };
+  const action = { meta: { ...meta, effect: "any_effect" } };
 
   expect(discard(error, action)).toEqual(false);
 });
